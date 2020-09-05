2c22f3ad2c120b1032668209ed7f1bf5d761f585
Mithil Poojary <mithil467@gmail.com>
2020-09-06 00:50:14 +0530

    Form post working

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const express = require("express");
 const dotenv = require("dotenv");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 // Load Config
 dotenv.config();
 
 const app = express();
 
-// Routes
-app.use("/", require("./routes/index"));
+// Bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server has initialized"));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => res.render("home", { title: "MitBin"}));
+router.get("/", (req, res) => res.render("home", { title: "MitBin" }));
 
-router.get("/sample", (req, res) => res.render("sample", { title: "Sample - MitBin"}));
+router.post("/", (req, res) => {
+  // Add to db
+  name = "smaple";
+  res.redirect("/" + name);
+});
+
+router.get("/:id", (req, res) => {
+  name = req.params.id;
+  language = "clike";
+  content = "Hi there";
+  res.render("sample", {
+    title: name + " - MitBin",
+    name: name,
+    language: language,
+    content: content,
+    createdOn: new Date(),
+    expiresOn: new Date(new Date().getTime() + 1440*60000),
+    helpers: {
+      localeDate: function (foo) {
+        return foo.toLocaleString();
+      },
+    },
+  });
+});
 
 module.exports = router;
