0a6582a46c5d2a6719c4fc2dc72d37d70497e7a4
Mithil Poojary <mithil467@gmail.com>
2020-09-05 22:40:57 +0530

    Add sample

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 router.get("/", (req, res) => res.render("home", { title: "MitBin"}));
 
+router.get("/sample", (req, res) => res.render("sample", { title: "Sample - MitBin"}));
+
 module.exports = router;
